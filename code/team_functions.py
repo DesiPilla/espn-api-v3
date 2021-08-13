4629a42afa535b2f562b932dc1d5343f924fb97b
DesiPilla <dmpilla@udel.edu>
2021-08-13 00:10:43 -0400

    Add updated luck index functions

--- code/team_functions.py
+++ code/team_functions.py
@@ -74,3 +74,74 @@ def total_bench_points(lineup: list):
     `lineup` is either BoxScore().away_lineup or BoxScore().home_lineup (a list of BoxPlayers)
     '''
     return np.sum([player.stats[1]['points'] for player in box_score.home_lineup if player.slot_position == 'BE'])
+
+
+
+
+
+
+
+''' ADVANCED STATS '''
+
+def get_weekly_luck_index(league: League, team: Team, week: int, num_teams: int):
+    ''' 
+    This function returns an index quantifying how 'lucky' a team was in a given week 
+    
+    Luck index:
+        50% probability of playing a team with a lower record
+        25% your play compared to previous weeks
+        25% opp's play compared to previous weeks
+    '''
+    opp = team.schedule[week-1]
+    
+    # Luck Index based on where the team and its opponent finished compared to the rest of the league  
+    rank = get_weekly_finish(league, team, week)
+    opp_rank = get_weekly_finish(league, opp, week)
+
+    if rank < opp_rank:                                # If the team won...
+        luck_index = 5 * (rank - 1) / (num_teams - 2)  # Odds of this team playing a team with a higher score than it
+    elif rank > opp_rank:                              # If the team lost or tied...
+        luck_index = -5 * (num_teams - rank) / (num_teams - 2)    # Odds of this team playing a team with a lower score than it
+
+    # If the team tied...
+    elif rank < (num_teams / 2):                                      
+        luck_index = -2.5 * (num_teams - rank - 1) / (num_teams - 2)  # They are only half as unlucky, because tying is not as bad as losing
+    else:
+        luck_index = 2.5 * (rank - 1) / (num_teams - 2)               # They are only half as lucky, because tying is not as good as winning
+
+
+    # Update luck index based on how team played compared to normal
+    team_score = team.scores[week - 1]
+    team_avg = np.mean(team.scores[:week])
+    team_std = np.std(team.scores[:week])
+    if team_std != 0:
+        # Get z-score of the team's performance
+        z = (team_score - team_avg) / team_std
+        
+        # Noramlize the z-score so that a performance 3 std dev's away from the mean has an effect of 2 points on the luck index
+        z_norm = z / (3*team_std) * 2.5
+        luck_index += z_norm
+
+    # Update luck index based on how opponent played compared to normal
+    opp_score = opp.scores[week - 1]
+    opp_avg = np.mean(opp.scores[:week])
+    opp_std = np.std(opp.scores[:week])
+    if team_std != 0:
+        # Get z-score of the team's performance
+        z = (opp_score - opp_avg) / opp_std
+        
+        # Noramlize the z-score so that a performance 3 std dev's away from the mean has an effect of 2 points on the luck index
+        z_norm = z / (3*opp_std) * 2.5
+        luck_index -= z_norm
+    
+    return luck_index / 10
+
+
+def get_season_luck_indices(league: League, week: int):
+    ''' This function returns an index quantifying how 'lucky' a team was all season long (up to a certain week) '''
+    luck_indices = {team:0 for team in league.teams}
+    for wk in range(1, week + 1):
+        # Update luck_index for each team
+        for team in league.teams:
+            luck_indices[team] += get_weekly_luck_index(team, week, len(league.teams))
+    return luck_indices
