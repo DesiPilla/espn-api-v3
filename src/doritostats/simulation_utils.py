033e7f4273b6a8f3b4a3bcae1c1e764e0d52ee4e
Desi Pilla <desi.m.pilla@gmail.com>
2022-11-07 11:05:48 -0500

    feat: add functions to build and sort current leaderboard

--- src/doritostats/simulation_utils.py
+++ src/doritostats/simulation_utils.py
@@ -4,6 +4,31 @@ from doritostats.fetch_utils import PseudoMatchup
 
 
 def get_score_prediction(team: Team):
+def sort_standings(
+    standings: pd.DataFrame, tie_breaker_rule: Optional[str] = None
+) -> pd.DataFrame:
+
+    # ASSUME Playoff Seeding Tie Breaker is "Total Points For"
+    # since API doesn't seem to get this data correctly
+    standings = standings.sort_values(by=["wins", "points_for"], ascending=False)
+
+    return standings
+
+
+def build_standings(league: League) -> pd.DataFrame:
+    standings = pd.DataFrame()
+    standings["team_id"] = [team.team_id for team in league.teams]
+    standings["team_name"] = [team.team_name for team in league.teams]
+    standings["wins"] = [team.wins for team in league.teams]
+    standings["ties"] = [team.ties for team in league.teams]
+    standings["losses"] = [team.losses for team in league.teams]
+    standings["points_for"] = [team.points_for for team in league.teams]
+    standings["points_against"] = [team.points_against for team in league.teams]
+    standings.set_index("team_id", inplace=True)
+
+    return sort_standings(standings)
+
+
     scores = np.array(team.scores)
     scores = scores[scores > 0][-6:]
     return np.random.normal(scores.mean(), scores.std())
