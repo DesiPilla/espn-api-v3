8995a0db032fb21cbfc9b2b8c397c77622364287
Desi Pilla <desi.m.pilla@gmail.com>
2022-11-11 10:11:11 -0500

    Update settings.py

--- doritostats/settings.py
+++ doritostats/settings.py
@@ -151,7 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
