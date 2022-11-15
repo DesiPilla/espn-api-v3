fe49f020fd118866ce99321521e450c1614fda51
Desi Pilla <desi.m.pilla@gmail.com>
2022-11-15 09:15:33 -0500

    fix: add host to `CSRF_TRUSTED_ORIGINS`

--- doritostats/settings.py
+++ doritostats/settings.py
@@ -35,10 +35,9 @@ DEBUG = True
 # DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["*"]
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://doritostats.up.railway.app",
+]
 
 # Application definition
 INSTALLED_APPS = [
