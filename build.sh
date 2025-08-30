ec0b475c031a39d17f5c54f2fce1028adf29f1c5
DesiPilla <desi.m.pilla@gmail.com>
2025-08-29 22:25:04 -0400

    build fixes

--- build.sh
+++ build.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
+set -e  # exit on any error
 
-# Exit on error
-set -o errexit  
-
-# Upgrade pip and install Poetry
-python -m pip install --upgrade pip
-pip install poetry
-
-# Export dependencies to requirements.txt for Railway
-poetry export -f requirements.txt --without-hashes -o requirements.txt
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Build React frontend
-echo "Building React frontend..."
+# Build frontend
 cd frontend
-npm install
+npm ci
 npm run build
 cd ..
 
-# Copy React build files to Django static folder
-cp -r frontend/build/* backend/static/
-
-# Run Django collectstatic
+# Collect static files for Django
 python manage.py collectstatic --noinput
-
-# Run migrations
-python manage.py migrate
