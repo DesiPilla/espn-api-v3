373afc9a91773765a7964c44a701a0e6ef2ee4ea
DesiPilla <desi.m.pilla@gmail.com>
2025-09-04 19:33:11 -0400

    fix: Stop tracking local traffic on google analytics

--- frontend/src/utils/google_analytics.js
+++ frontend/src/utils/google_analytics.js
@@ -1,17 +1,22 @@
 export const initGoogleAnalytics = () => {
-  if (process.env.NODE_ENV !== 'production') return
+  if (
+      window.location.hostname != "localhost" &&
+      window.location.hostname != "127.0.0.1"
+  ) {
+      const script1 = document.createElement("script");
+      script1.async = true;
+      script1.src = "https://www.googletagmanager.com/gtag/js?id=G-45P4BQ6XJR";
+      document.head.appendChild(script1);
 
-  const script1 = document.createElement('script')
-  script1.async = true
-  script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-45P4BQ6XJR'
-  document.head.appendChild(script1)
-
-  const script2 = document.createElement('script')
-  script2.innerHTML = `
+      const script2 = document.createElement("script");
+      script2.innerHTML = `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-45P4BQ6XJR');
-  `
-  document.head.appendChild(script2)
+  `;
+      document.head.appendChild(script2);
+  }
+
+  
 }
