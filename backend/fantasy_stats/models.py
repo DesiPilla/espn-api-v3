3e6a30c9e1106b51b9f47486a089d8798a209b48
DesiPilla <desi.m.pilla@gmail.com>
2025-09-28 23:16:06 -0400

    fix: Add soft delete for invalid leagues
    Many leagues from previous years were added erroneously or are currently broken. This adds a `deleted` column to the `LeagueInfo` model and sets the value to `true` for all broken leagues. These will be automatically filtered out and no longer selectable from the UI

--- backend/fantasy_stats/models.py
+++ backend/fantasy_stats/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        # soft delete instead of hard delete
+        return super().update(deleted=True)
+
+    def hard_delete(self):
+        # if you need a true delete
+        return super().delete()
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        # by default, filter out deleted=True
+        return super().get_queryset().filter(deleted=False)
+
+    def all_with_deleted(self):
+        # optional: get everything, including deleted
+        return super().get_queryset()
+
+
 # Create your models here.
 class LeagueInfo(models.Model):
     league_id = models.IntegerField()
@@ -9,6 +29,14 @@ class LeagueInfo(models.Model):
     espn_s2 = models.CharField(max_length=500)
     league_name = models.CharField(max_length=50, default="<Name Missing>")
     created_date = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()  # default manager (excludes deleted)
+    all_objects = models.Manager()  # backup manager (includes deleted)
+
+    class Meta:
+        # if you query this model without specifying manager, deleted=False is implied
+        default_manager_name = "objects"
 
     def __str__(self):
         return "{} ({})".format(self.league_id, self.league_year)
