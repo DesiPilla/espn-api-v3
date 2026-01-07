302e1f38dfff528ec1d4ee3230960ec7a5fb72ac
DesiPilla <desi.m.pilla@gmail.com>
2026-01-07 15:49:00 -0500

    feat: add playoff pool initial commit

---
+++ backend/playoff_pool/players.py
@@ -0,0 +1,85 @@
+from typing import List
+
+import nflreadpy as nfl
+import pandas as pd
+
+from backend.playoff_pool.scoring import (
+    RELEVANT_SCORING_STATS,
+    RELEVANT_DEFENSIVE_SCORING_STATS,
+    calculate_fantasy_points,
+)
+
+
+def get_all_playoff_players(
+    year: int, positions_to_keep: List[str] = ["QB", "RB", "WR", "TE", "K", "DST"]
+) -> pd.DataFrame:
+    # Load the schedules for the given year
+    nfl_schedule = nfl.load_schedules(year)
+
+    # Get the game IDs for all playoff games
+    playoff_game_ids = nfl_schedule.filter(pl.col("game_type") != "REG")[
+        "game_id"
+    ].to_list()
+
+    # Get the unique playoff teams from the game IDs
+    playoff_teams = set(
+        [
+            team
+            for gid in playoff_game_ids
+            for team in (gid.split("_")[-2], gid.split("_")[-1])
+        ]
+    )
+
+    # ------------ Get list of individual players ------------
+    # Load player game-level stats for multiple seasons
+    player_stats = nfl.load_player_stats([year]).to_pandas()
+    player_stats["fantasy_points"] = player_stats.apply(
+        lambda row: calculate_fantasy_points(row, RELEVANT_SCORING_STATS),
+        axis=1,
+    )
+
+    # Get the total fantasy points for each player in the playoff rosters
+    playoff_skill_players = (
+        pd.merge(
+            playoff_rosters,
+            player_stats.groupby("player_id", as_index=False).sum(numeric_only=True)[
+                ["player_id", "fantasy_points"]
+            ],
+            left_on="gsis_id",
+            right_on="player_id",
+            how="left",
+        )
+        .fillna(0)
+        .sort_values(["position", "fantasy_points"], ascending=[True, False])
+    )
+
+    # ------------ Get list of Defenses ------------
+    # Load all available team level stats
+    team_stats = nfl.load_team_stats(seasons=[year]).to_pandas()
+
+    playoff_team_stats = team_stats[
+        team_stats["team"].isin(playoff_teams) & (team_stats["season_type"] == "REG")
+    ]
+
+    playoff_team_stats["fantasy_points"] = playoff_team_stats.apply(
+        lambda row: calculate_fantasy_points(row, RELEVANT_DEFENSIVE_SCORING_STATS),
+        axis=1,
+    )
+
+    playoff_dst_stats = (
+        playoff_team_stats.groupby("team")[["fantasy_points"]].sum().reset_index()
+    )
+    playoff_dst_stats["gsis_id"] = playoff_dst_stats["team"]
+    playoff_dst_stats["full_name"] = playoff_dst_stats["team"] + " D/ST"
+    playoff_dst_stats["position"] = "DST"
+    playoff_dst_stats["player_id"] = playoff_dst_stats["gsis_id"]
+
+    # Combine skill players and DSTs
+    playoff_players = pd.concat([playoff_skill_players, playoff_dst_stats])
+
+    # Filter by positions to keep
+    playoff_players_filtered = playoff_players[
+        playoff_players["position"].isin(positions_to_keep)
+    ]
+
+    return playoff_players_filtered
---
+++ backend/playoff_pool/scoring.py
@@ -0,0 +1,180 @@
+SCORING_MULTIPLIERS = {
+    # -------------------
+    # Passing
+    # -------------------
+    "completions": 0,
+    "attempts": 0,
+    "passing_yards": 1 / 25,
+    "passing_tds": 4,
+    "passing_interceptions": -2,
+    "sacks_suffered": 0,
+    "sack_yards_lost": 0,
+    "sack_fumbles": 0,
+    "sack_fumbles_lost": -2,
+    "passing_air_yards": 0,
+    "passing_yards_after_catch": 0,
+    "passing_first_downs": 0,
+    "passing_epa": 0,
+    "passing_cpoe": 0,
+    "passing_2pt_conversions": 2,
+    "pacr": 0,
+    # -------------------
+    # Rushing
+    # -------------------
+    "carries": 0,
+    "rushing_yards": 1 / 10,
+    "rushing_tds": 6,
+    "rushing_fumbles": 0,
+    "rushing_fumbles_lost": -2,
+    "rushing_first_downs": 0,
+    "rushing_epa": 0,
+    "rushing_2pt_conversions": 2,
+    # -------------------
+    # Receiving
+    # -------------------
+    "receptions": 0,  # non-PPR
+    "targets": 0,
+    "receiving_yards": 1 / 10,
+    "receiving_tds": 6,
+    "receiving_fumbles": 0,
+    "receiving_fumbles_lost": -2,
+    "receiving_air_yards": 0,
+    "receiving_yards_after_catch": 0,
+    "receiving_first_downs": 0,
+    "receiving_epa": 0,
+    "receiving_2pt_conversions": 2,
+    "racr": 0,
+    "target_share": 0,
+    "air_yards_share": 0,
+    "wopr": 0,
+    # -------------------
+    # Defense / IDP
+    # -------------------
+    "special_teams_tds": 6,
+    "def_tackles_solo": 1,
+    "def_tackles_with_assist": 0,
+    "def_tackle_assists": 0.5,
+    "def_tackles_for_loss": 1,
+    "def_tackles_for_loss_yards": 0,
+    "def_fumbles_forced": 2,
+    "def_sacks": 2,
+    "def_sack_yards": 0,
+    "def_qb_hits": 0,
+    "def_interceptions": 2,
+    "def_interception_yards": 0,
+    "def_pass_defended": 1,
+    "def_tds": 6,
+    "def_fumbles": 2,  # recovered
+    "def_safeties": 2,
+    # -------------------
+    # Fumble recoveries
+    # -------------------
+    "fumble_recovery_own": 0,
+    "fumble_recovery_yards_own": 0,
+    "fumble_recovery_opp": 2,
+    "fumble_recovery_yards_opp": 0,
+    "fumble_recovery_tds": 6,
+    # -------------------
+    # Returns / misc
+    # -------------------
+    "misc_yards": 0,
+    "penalties": 0,
+    "penalty_yards": 0,
+    "punt_returns": 0,
+    "punt_return_yards": 0,
+    "kickoff_returns": 0,
+    "kickoff_return_yards": 0,
+    # -------------------
+    # Kicking
+    # -------------------
+    "fg_made": 3,  # overridden by distance below
+    "fg_att": 0,
+    "fg_missed": -1,
+    "fg_blocked": 0,
+    "fg_long": 0,
+    "fg_pct": 0,
+    "fg_made_0_19": 3,
+    "fg_made_20_29": 3,
+    "fg_made_30_39": 3,
+    "fg_made_40_49": 4,
+    "fg_made_50_59": 5,
+    "fg_made_60_": 5,
+    "fg_missed_0_19": -1,
+    "fg_missed_20_29": -1,
+    "fg_missed_30_39": -1,
+    "fg_missed_40_49": -1,
+    "fg_missed_50_59": -1,
+    "fg_missed_60_": -1,
+    # "fg_made_list": 0, # Lists will break the calculation
+    # "fg_missed_list": 0, # Lists will break the calculation
+    # "fg_blocked_list": 0, # Lists will break the calculation
+    "fg_made_distance": 0,
+    "fg_missed_distance": 0,
+    "fg_blocked_distance": 0,
+    "pat_made": 1,
+    "pat_att": 0,
+    "pat_missed": -1,
+    "pat_blocked": 0,
+    "pat_pct": 0,
+    "gwfg_made": 0,
+    "gwfg_att": 0,
+    "gwfg_missed": 0,
+    "gwfg_blocked": 0,
+    "gwfg_distance": 0,
+}
+
+DEFENSE_SCORING_MULTIPLIERS = {
+    # Touchdowns
+    "special_teams_tds": 6,
+    "def_tds": 6,
+    "fumble_recovery_tds": 6,
+    # Tackles (not scored in ESPN standard D/ST)
+    "def_tackles_solo": 0,
+    "def_tackles_with_assist": 0,
+    "def_tackle_assists": 0,
+    "def_tackles_for_loss": 0,
+    "def_tackles_for_loss_yards": 0,
+    # Turnovers & pressure
+    "def_fumbles_forced": 0,  # ESPN only scores fumble recoveries
+    "def_fumbles": 2,  # fumbles recovered
+    "def_interceptions": 2,
+    "def_interception_yards": 0,
+    "def_sacks": 1,
+    "def_sack_yards": 0,
+    "def_qb_hits": 0,
+    # Coverage / misc defense
+    "def_pass_defended": 0,
+    "def_safeties": 2,
+    # Fumble recoveries (by possession)
+    "fumble_recovery_own": 0,
+    "fumble_recovery_yards_own": 0,
+    "fumble_recovery_opp": 2,
+    "fumble_recovery_yards_opp": 0,
+    # Returns (yards not scored)
+    "punt_returns": 0,
+    "punt_return_yards": 0,
+    "kickoff_returns": 0,
+    "kickoff_return_yards": 0,
+    # Misc / non-scoring
+    "misc_yards": 0,
+    "penalties": 0,
+    "penalty_yards": 0,
+    "timeouts": 0,
+}
+
+RELEVANT_SCORING_STATS = {
+    stat: multiplier for stat, multiplier in SCORING_MULTIPLIERS.items() if multiplier != 0
+}
+
+RELEVANT_DEFENSIVE_SCORING_STATS = {
+    stat: multiplier
+    for stat, multiplier in DEFENSE_SCORING_MULTIPLIERS.items()
+    if multiplier != 0
+}
+
+
+def calculate_fantasy_points(row, scoring_dict):
+    points = 0
+    for stat, value in scoring_dict.items():
+        points += (row.get(stat) or 0) * value
+    return points
